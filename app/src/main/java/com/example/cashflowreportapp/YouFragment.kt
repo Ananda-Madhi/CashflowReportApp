3a7a278a534ee648eda9f38dbc4b2db464dab227
Ananda-Madhi <147123700+Ananda-Madhi@users.noreply.github.com>
2025-10-16 14:29:49 +0700

    Saldo Pribadi harusnya dah bener

--- app/src/main/java/com/example/cashflowreportapp/YouFragment.kt
+++ app/src/main/java/com/example/cashflowreportapp/YouFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
 import com.example.cashflowreportapp.databinding.FragmentYouBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -33,17 +32,20 @@ class YouFragment : Fragment() {
         binding.tvUserEmail.text = currentUser?.email ?: "Pengguna tidak ditemukan"
 
         binding.btnViewBalance.setOnClickListener {
-            Toast.makeText(context, "Fitur ini sedang dalam pengembangan", Toast.LENGTH_SHORT)
-                .show()
+            val bundle = Bundle().apply {
+                putString("account_name", "You")
+            }
+            findNavController().navigate(R.id.action_youFragment_to_accountTransactionsFragment, bundle)
         }
 
         binding.btnLogout.setOnClickListener {
             auth.signOut()
             Toast.makeText(requireContext(), "Anda telah keluar", Toast.LENGTH_SHORT).show()
 
-            val navOptions = NavOptions.Builder()
-                .setPopUpTo(R.id.nav_graph, true)
-                .build()
+            findNavController().navigate(R.id.loginFragment)
+            val navOptions = androidx.navigation.NavOptions.Builder()
+            .setPopUpTo(R.id.nav_graph, true)
+            .build()
             findNavController().navigate(R.id.loginFragment, null, navOptions)
         }
     }
